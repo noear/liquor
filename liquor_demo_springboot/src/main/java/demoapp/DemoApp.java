ae7774185c0e49f70da7e6dcdc9106f3d0acb222
noear <noear@live.cn>
2021-05-31 20:20:35 +0800

    1.0.0

---
+++ liquor_demo_springboot/src/main/java/demoapp/DemoApp.java
@@ -0,0 +1,46 @@
+package demoapp;
+
+import org.noear.liquor.DynamicCompiler;
+import org.noear.solon.Solon;
+import org.noear.solon.Utils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author noear 2021/5/31 created
+ */
+@SpringBootApplication
+public class DemoApp {
+    public static void main(String[] args) throws Exception{
+
+        SpringApplication.run(DemoApp.class, args);
+
+        System.out.println("------------------ test1.无第三方包引入 ------------------");
+        compiler_test1();
+
+        System.out.println("------------------ test2.有第三方包引入(验证 spring-boot-maven-plugin 打包是否运行正常) ------------------");
+        compiler_test2();
+    }
+
+    public static void compiler_test1() {
+        String className = "com.demo.proxy.MyClass";
+        String classCode = Utils.getResourceAsString("codefile/MyClass1.txt", null);
+
+        System.out.println(classCode);
+        DynamicCompiler compiler = new DynamicCompiler();
+        compiler.addSource(className, classCode);
+        Object instance = compiler.build().get(className);
+        System.out.println(instance);
+    }
+
+    public static void compiler_test2()  {
+        String className = "com.demo.proxy.MyClass";
+        String classCode = Utils.getResourceAsString("codefile/MyClass2.txt", null);
+
+        System.out.println(classCode);
+        DynamicCompiler compiler = new DynamicCompiler();
+        compiler.addSource(className, classCode);
+        Object instance = compiler.build().get(className);
+        System.out.println(instance);
+    }
+}
---
+++ liquor_demo_springboot/src/main/java/demoapp/DemoApp2.java
@@ -0,0 +1,28 @@
+package demoapp;
+
+import org.noear.liquor.DynamicCompiler;
+
+import java.util.Map;
+
+/**
+ * @author noear 2021/5/31 created
+ */
+public class DemoApp2 {
+    public static void main(String[] args) {
+        String className = "com.demo.proxy.MyClass";
+        String classCode = "package com.demo.proxy;\n" +
+                "\n" +
+                "public class MyClass {\n" +
+                "\n" +
+                "    public String say(String str){\n" +
+                "        return \"hello\"+str;\n" +
+                "    }\n" +
+                "}";
+
+        DynamicCompiler compiler = new DynamicCompiler();
+        compiler.addSource(className, classCode);
+        Map<String, Class<?>> classMap = compiler.build();
+
+        System.out.println(classMap.get(className));
+    }
+}
