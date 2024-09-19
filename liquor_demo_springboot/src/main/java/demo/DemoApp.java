bb6dbd80860984f886d4a876e6e92071fd0c1cd9
noear <noear@live.cn>
2024-09-19 16:51:24 +0800

    demo

--- liquor_demo_springboot/src/main/java/demo/DemoApp.java
+++ liquor_demo_springboot/src/main/java/demo/DemoApp.java
@@ -1,7 +1,7 @@
 package demo;
 
 import org.noear.liquor.DynamicCompiler;
-import org.noear.solon.Utils;
+import org.noear.solon.core.util.ResourceUtil;
 import org.springframework.boot.SpringApplication;
 
 /**
@@ -9,19 +9,37 @@ import org.springframework.boot.SpringApplication;
  */
 public class DemoApp {
     public static void main(String[] args) throws Exception {
-
         SpringApplication.run(DemoApp.class, args);
 
-        System.out.println("------------------ test1.无第三方包引入 ------------------");
+        System.out.println("------------------ test0.无第三方包引入（纯代码） ------------------");
+        compiler_test0();
+
+        System.out.println("------------------ test1.无第三方包引入（读取文件）------------------");
         compiler_test1();
 
         System.out.println("------------------ test2.有第三方包引入(验证 spring-boot-maven-plugin 打包是否运行正常) ------------------");
         compiler_test2();
     }
 
+    public static void compiler_test0() throws Exception {
+        String className = "HelloWorld";
+        String classCode = "public class HelloWorld { " +
+                "   public static void helloWorld() { " +
+                "       System.out.println(\"Hello, world!\"); " +
+                "   } " +
+                "}";
+
+        DynamicCompiler compiler = new DynamicCompiler();
+        //添加源码（可多个）并构建
+        compiler.addSource(className, classCode).build();
+
+        Class<?> clazz = compiler.getClassLoader().loadClass(className);
+        clazz.getMethod("helloWorld").invoke(null);
+    }
+
     public static void compiler_test1() throws Exception {
         String className = "com.demo.proxy.MyClass";
-        String classCode = Utils.getResourceAsString("codefile/MyClass1.txt", null);
+        String classCode = ResourceUtil.getResourceAsString("codefile/MyClass1.txt", null);
 
         System.out.println(classCode);
         DynamicCompiler compiler = new DynamicCompiler();
@@ -33,7 +51,7 @@ public class DemoApp {
 
     public static void compiler_test2() throws Exception {
         String className = "com.demo.proxy.MyClass";
-        String classCode = Utils.getResourceAsString("codefile/MyClass2.txt", null);
+        String classCode = ResourceUtil.getResourceAsString("codefile/MyClass2.txt", null);
 
         System.out.println(classCode);
         DynamicCompiler compiler = new DynamicCompiler();
