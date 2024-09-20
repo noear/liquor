3cec7f5a436aaf937c38df2980151029cfe01321
noear <noear@live.cn>
2024-09-20 10:41:08 +0800

    1.2.4

---
+++ liquor_benchmark/README.md
@@ -0,0 +1,29 @@
+
+特点比较：
+
+|    | liquor      | janino      |
+|----|-------------|-------------|
+| 编译 | 可以复用编译器     | 每次要新建       | 
+|    | 每次新建编译器，成本高 | 每次新建编译器，成本低 |
+
+实现以下效果，liquor 比 janino 快10多倍。
+
+```java
+public static void expr_liquor_11(int count) throws Exception {
+    long start = System.currentTimeMillis();
+
+    //可以复用
+    org.noear.liquor.eval.ExpressionEvaluator evaluator = new org.noear.liquor.eval.ExpressionEvaluator();
+    for (int i = 0; i < count; i++) {
+        evaluator.eval("1+1");
+        evaluator.eval("1+2");
+        evaluator.eval("1+3");
+        evaluator.eval("1+4");
+        evaluator.eval("1+5");
+    }
+
+    System.out.println("liquor::" + (System.currentTimeMillis() - start));
+}
+
+expr_liquor_11(1000);
+```
---
+++ liquor_benchmark/src/main/java/benchmark/Case1.java
@@ -0,0 +1,63 @@
+package benchmark;
+
+
+
+public class Case1 {
+    public static void main(String[] args) throws Exception {
+        expr_janino_11(1000);
+        expr_liquor_11(1000);
+
+        expr_janino_11(1000);
+        expr_liquor_11(1000);
+
+        expr_janino_11(1000);
+        expr_liquor_11(1000);
+    }
+
+    public static void expr_janino_11(int count) throws Exception {
+        long start = System.currentTimeMillis();
+
+
+        //不能复用
+        org.codehaus.janino.ExpressionEvaluator evaluator;
+        for (int i = 0; i < count; i++) {
+            evaluator = new org.codehaus.janino.ExpressionEvaluator();
+            evaluator.cook("1+1");
+            evaluator.evaluate();
+
+            evaluator = new org.codehaus.janino.ExpressionEvaluator();
+            evaluator.cook("1+2");
+            evaluator.evaluate();
+
+            evaluator = new org.codehaus.janino.ExpressionEvaluator();
+            evaluator.cook("1+3");
+            evaluator.evaluate();
+
+            evaluator = new org.codehaus.janino.ExpressionEvaluator();
+            evaluator.cook("1+4");
+            evaluator.evaluate();
+
+            evaluator = new org.codehaus.janino.ExpressionEvaluator();
+            evaluator.cook("1+5");
+            evaluator.evaluate();
+        }
+
+        System.out.println("janino::" + (System.currentTimeMillis() - start));
+    }
+
+    public static void expr_liquor_11(int count) throws Exception {
+        long start = System.currentTimeMillis();
+
+        //可以复用
+        org.noear.liquor.eval.ExpressionEvaluator evaluator = new org.noear.liquor.eval.ExpressionEvaluator();
+        for (int i = 0; i < count; i++) {
+            evaluator.eval("1+1");
+            evaluator.eval("1+2");
+            evaluator.eval("1+3");
+            evaluator.eval("1+4");
+            evaluator.eval("1+5");
+        }
+
+        System.out.println("liquor::" + (System.currentTimeMillis() - start));
+    }
+}
