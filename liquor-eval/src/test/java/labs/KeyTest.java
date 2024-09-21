21f257c8a2c4ecc8546afd1f2ad7188fb7c4c5f0
noear <noear@live.cn>
2024-09-21 10:56:17 +0800

    优化 ParamSpec 与 CodeSpec hash算法

--- liquor-eval/src/test/java/labs/KeyTest.java
+++ liquor-eval/src/test/java/labs/KeyTest.java
@@ -1,6 +1,7 @@
 package labs;
 
 import org.noear.liquor.eval.CodeSpec;
+import org.noear.liquor.eval.ParamSpec;
 
 /**
  * @author noear 2024/9/20 created
@@ -9,19 +10,16 @@ public class KeyTest {
     public static void main(String[] args) {
         System.out.println(new CodeSpec("1+1").hashCode());
         System.out.println(new CodeSpec("1+1").hashCode());
-        System.out.println(new CodeSpec("1+1").hashCode());
-        System.out.println(new CodeSpec("1+1").hashCode());
-        System.out.println(new CodeSpec("1+1").hashCode());
-        System.out.println(new CodeSpec("1+2").hashCode());
-        System.out.println(new CodeSpec("1+2").hashCode());
-        System.out.println(new CodeSpec("1+2").hashCode());
-        System.out.println(new CodeSpec("1+2").hashCode());
-        System.out.println(new CodeSpec("1+2").hashCode());
-        System.out.println(new CodeSpec("1+2").hashCode());
+        System.out.println("-------------");
+        assert new CodeSpec("1+1").hashCode() == new CodeSpec("1+1").hashCode();
 
-        System.out.println("1+1".hashCode());
-        System.out.println("1+1".hashCode());
-        System.out.println("1+2".hashCode());
-        System.out.println("1+2".hashCode());
+        System.out.println(new CodeSpec("1+1").parameters(new ParamSpec("name", String.class)).hashCode());
+        System.out.println(new CodeSpec("1+1").parameters(new ParamSpec("name", String.class)).hashCode());
+        System.out.println("-------------");
+        assert new CodeSpec("1+1").parameters(new ParamSpec("name", String.class)).hashCode() == new CodeSpec("1+1").parameters(new ParamSpec("name", String.class)).hashCode();
+
+        System.out.println(new ParamSpec("name", String.class).hashCode());
+        System.out.println(new ParamSpec("name", String.class).hashCode());
+        assert new ParamSpec("name", String.class).hashCode() == new ParamSpec("name", String.class).hashCode();
     }
 }
