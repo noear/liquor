85b33c9e4d4277ffa778e85208914be0dd25c13c
noear <noear@live.cn>
2025-03-20 12:28:03 +0800

    Update DynamicDemoScript.java

--- demo_dynamic_script/DynamicDemoScript.java
+++ demo_dynamic_script/DynamicDemoScript.java
@@ -2,7 +2,7 @@ package com.demo.script;
 
 import java.util.Map;
 
-//此脚本由 demo_dynamic_compiling_and_debugging_solon 动态加载，且是可调试的
+//此脚本由 demo_dynamic_compiling_and_debugging_solon 动态加载，且是可调试的 
 public class DynamicDemoScript {
     public String hello(Map<String, Object> params) {
         System.out.println("Hello, Dynamic Script!1");
