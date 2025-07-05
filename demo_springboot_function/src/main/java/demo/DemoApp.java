5ec09209aa5f035400bb6963c7e728c4d2d5202e
phanes <5411232+phanes@user.noreply.gitee.com>
2025-07-05 16:18:58 +0800

    新增 Spring Boot 动态编译和 Bean 管理演示

---
+++ demo_springboot_function/src/main/java/demo/DemoApp.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 启动类
+ *
+ * @author phanes
+ */
+@SpringBootApplication
+public class DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+
+}
