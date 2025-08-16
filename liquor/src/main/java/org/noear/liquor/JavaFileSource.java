6020deadf11a7231f54bb8c90f095bc457b5071c
noear <noear@live.cn>
2025-08-16 17:41:04 +0800

    1.6.2

--- liquor/src/main/java/org/noear/liquor/JavaFileSource.java
+++ liquor/src/main/java/org/noear/liquor/JavaFileSource.java
@@ -3,6 +3,7 @@ package org.noear.liquor;
 import javax.tools.SimpleJavaFileObject;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
@@ -14,14 +15,25 @@ import java.nio.file.Files;
  */
 public class JavaFileSource extends SimpleJavaFileObject {
     private final File fileSource;
+    private final Charset charset;
 
     public JavaFileSource(File fileSource) {
+        this(fileSource, null);
+    }
+
+    public JavaFileSource(File fileSource, Charset charset) {
         super(fileSource.toURI(), Kind.SOURCE);
         this.fileSource = fileSource;
+
+        if (charset == null) {
+            this.charset = StandardCharsets.UTF_8;
+        } else {
+            this.charset = charset;
+        }
     }
 
     @Override
     public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
-        return new String(Files.readAllBytes(fileSource.toPath()), StandardCharsets.UTF_8);
+        return new String(Files.readAllBytes(fileSource.toPath()), charset);
     }
 }
